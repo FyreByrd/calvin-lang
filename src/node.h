bf3fdd58ad8a0658472be7922432be795b4b7a00
FyreByrd <aejones4gm@gmail.com>
2023-12-09 12:33:35 -0500

    add node and types

---
+++ src/node.h
@@ -0,0 +1,331 @@
+#pragma once
+#include "types.h"
+#include <vector>
+
+class node {
+private:
+    std::shared_ptr<meta> t_meta;
+public:
+    node(std::shared_ptr<meta> m) { 
+        if (!m) debug::exit(debug::err() << "Null meta!");
+        t_meta = m; }
+    std::shared_ptr<meta> type_meta() const { return t_meta; }
+    void setMeta(std::shared_ptr<meta> m) { t_meta = m; }
+
+    virtual void print(std::ostream& out, size_t indent=0, std::string pre="") const {
+        //debug::log() << "node print!" << std::endl;
+        out << utils::prefix("\t", indent) << pre;
+        //debug::log() << "node print2!" << std::endl;
+        type_meta()->print(out);
+    }
+};
+
+template <typename T>
+class constant : virtual public node {
+private:
+    T t_value;
+public:
+    constant(T value, std::shared_ptr<meta> m) : node(m) { this->t_value = value; }
+    virtual T value() const { return t_value; }
+    virtual void setValue(T value) { this->t_value = value; }
+
+    virtual void print(std::ostream& out, size_t indent=0, std::string pre="") const {
+        node::print(out, indent, pre);
+        out << " " << t_value;
+    }
+};
+
+class string_constant: virtual public node {
+private:
+    std::string val;
+    std::string escape(std::string s) {
+        std::string ret = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '\\')
+                if (s[i+1] == '\\') {
+                    ret += "\\\\";
+                    i++; continue;
+                }
+                else
+                    ret += "\\\\";
+            else
+                ret += s[i];
+        }
+        return ret;
+    }
+public:
+    string_constant(std::string name) : node (std::make_shared<meta>(meta("string"))) {
+        val = escape(name);
+    }
+
+    std::string value() const { return val; }
+
+    void setValue(std::string val) {
+        this->val = escape(val);
+    }
+
+    void print(std::ostream& out, size_t indent=0, std::string pre="") const {
+        out << "\"" << val << "\"";
+    }
+};
+
+class list: virtual public node {
+private:
+    std::vector<std::shared_ptr<node>> members;
+public:
+    list(std::shared_ptr<meta> type = std::make_shared<meta>(LIST, meta::string(LIST)))
+     : node(type) { 
+        members = std::vector<std::shared_ptr<node>>(); 
+    }
+    list(std::shared_ptr<node> first) : node(std::make_shared<meta>(first->type_meta())) {
+        if (!first) debug::err() << "null first!" << std::endl;
+        members.push_back(first);
+    }
+    
+    size_t length() const { return members.size(); }
+
+    int append(std::shared_ptr<node> node) { 
+        if (!node) debug::err() << "null append!" << std::endl;
+        members.push_back(node); 
+        return members.size() - 1; 
+    }
+    void extend(std::shared_ptr<list> src) {
+        for (auto e: src->members) {
+            if (!e) debug::err() << "null extend!" << std::endl;
+            members.push_back(e);
+        }
+        src->members.clear();
+    }
+
+    std::shared_ptr<node> at(size_t i) { return members.at(i); }
+
+    void print(std::ostream& out, size_t indent=0, std::string pre="") const {
+        //debug::log() << "here!" << std::endl;
+        node::print(out, indent, pre);
+        out << " : " << length() << "[" << std::endl;
+        for (auto m: members) {
+            m->print(out, indent+1, pre);
+            out << std::endl;
+        }
+        if (length() <= 0) out << utils::prefix("\t", indent+1) << pre << "no members" << std::endl;
+        out << utils::prefix("\t", indent) << pre << "]" << std::endl;
+    }
+};
+
+class type_node: virtual public node {
+public:
+    type_node(std::shared_ptr<meta> type) : node(type) {
+        if (!type) {
+            debug::exit(debug::err() << "type_node must be created with a TypeMeta argument!");
+        }
+    }
+
+    void print(std::ostream& out, size_t indent=0, std::string pre="") const {
+        out << utils::prefix("\t", indent) << pre;
+        node::print(out);
+    }
+};
+
+class ID: virtual public node {
+private:
+    std::string id;
+public:
+    ID(std::string name, std::shared_ptr<meta> t) : node(t) {
+        id = name;
+    }
+    std::string name() const { return id; }
+    void setName(std::string name) { id = name; }
+
+    void print(std::ostream& out, size_t indent=0, std::string pre="") const {
+        out << utils::prefix("\t", indent) << pre << name();
+    }
+};
+
+typedef enum {
+    //arithmetic
+    ADD, SUB, MUL, DIV, MOD,
+    //bitwise
+    BNOT, BAND, BOR, BXOR,
+    //shift
+    LSL, LSR, ASR,
+    //assign
+    EQU,
+    //boolean
+    LNOT, LAND, LOR,
+    //comparison
+    EE, NE, GE, LE, LT, GT,
+    //cast
+    CAST,
+    //return
+    RET
+} ExprType;
+
+typedef enum {
+    ARITH,
+    BITS,
+    ASSGN,
+    COMP,
+    PROC
+} ExprClass;
+
+class expr: virtual public node {
+private:
+    std::shared_ptr<node> left_arg;
+    std::shared_ptr<node> right_arg;
+    ExprType e_type;
+    static void rotate(expr* e) {
+        expr* er = dynamic_cast<expr*>(&(*(e->right_arg)));
+        if (!er) return;
+        ExprType et = e->e_type;
+        std::shared_ptr<node> l = e->left_arg;
+        e->right_arg = er->right_arg;
+        e->e_type = er->e_type;
+        e->left_arg = std::make_shared<expr>(et, l, er->left_arg);
+    }
+    std::shared_ptr<meta> eval() {
+        std::shared_ptr<meta> ret;
+        switch (eclass()) {
+            case ARITH:
+                if (std::shared_ptr<expr> e = std::dynamic_pointer_cast<expr>(right_arg)) {
+                    if ((e_type == MUL || e_type == DIV || e_type == MOD) 
+                        && (e->e_type == ADD || e->e_type == SUB)) {
+                        rotate(this);
+                    }
+                }
+                if (*(left_arg->type_meta()) != *(right_arg->type_meta())) {
+                    if (meta::canCast(left_arg->type_meta(), right_arg->type_meta())) {
+                        right_arg = std::make_shared<expr>(CAST, 
+                            std::make_shared<type_node>(left_arg->type_meta()),
+                            right_arg);
+                    }
+                    else if (meta::canCast(right_arg->type_meta(), left_arg->type_meta())) {
+                        left_arg = std::make_shared<expr>(CAST, 
+                            std::make_shared<type_node>(right_arg->type_meta()),
+                            left_arg);
+                    }
+                    else debug::exit(debug::err() << "TypeError: Impossible to cast between "
+                        << *left_arg->type_meta() << " and " << *right_arg->type_meta() << "!");
+                }
+                ret = left_arg->type_meta();
+            case BITS:
+                if (right_arg->type_meta()->dataClass() != INTEGRAL
+                    || (left_arg && left_arg->type_meta()->dataClass() != INTEGRAL))
+                    debug::exit(debug::err() << "TypeError: Bitwise operations are only valid for integral types!");
+                if (left_arg) ret = left_arg->type_meta();
+                else ret = right_arg->type_meta();
+                break;
+            case ASSGN:
+                ret = left_arg->type_meta();
+                if (right_arg) {
+                    if (*left_arg->type_meta() != *right_arg->type_meta())
+                        if (meta::canCast(left_arg->type_meta(), right_arg->type_meta()))
+                            right_arg = std::make_shared<expr>(CAST, 
+                                std::make_shared<type_node>(left_arg->type_meta()),
+                                right_arg);
+                        else debug::exit(debug::err() << "TypeError: Impossible to cast "
+                            << *right_arg->type_meta() << " to " << *left_arg->type_meta() << "!");
+                }
+            case COMP:
+                ret = std::make_shared<meta>("bool");
+                break;
+            case PROC:
+                ret = left_arg->type_meta();
+                break;
+        }
+        return ret;
+    }
+public:
+    expr(ExprType etype, std::shared_ptr<node> left, std::shared_ptr<node> right) 
+        : node(std::make_shared<meta>("int")) {
+        left_arg = left;
+        right_arg = right;
+        e_type = etype;
+        setMeta(eval());
+    }
+    std::shared_ptr<node> left() const { return left_arg; }
+    std::shared_ptr<node> right() const { return right_arg; }
+    ExprType etype() const { return e_type; }
+    ExprClass eclass() const {
+        if (e_type <= MOD) return ARITH;
+        else if (e_type >= BNOT && e_type <= ASR) return BITS;
+        else if (e_type == EQU) return ASSGN;
+        else if (e_type >= EE && e_type <= GT) return COMP;
+        else return PROC;
+    }
+    void setLeft(std::shared_ptr<node> left) { left_arg = left; setMeta(eval()); }
+    void setRight(std::shared_ptr<node> right) { right_arg = right; setMeta(eval()); }
+    void setType(ExprType type) { e_type = type; }
+    static std::string string(ExprType et) {
+        switch (et) {
+            //arithmetic
+            case ADD:   return "+";
+            case SUB:   return "-";
+            case MUL:   return "*";
+            case DIV:   return "/";
+            case MOD:   return "%";
+            //bitwise
+            case BNOT:  return "~";
+            case BAND:  return "&";
+            case BOR:   return "|";
+            case BXOR:  return "^";
+            //shift
+            case LSL:   return "<<";
+            case LSR:   return ">>";
+            case ASR:   return ">>>";
+            //assign
+            case EQU:   return "=";
+            //boolean
+            case LNOT:  return "not";
+            case LAND:  return "and";
+            case LOR:   return "or";
+            //comparison
+            case EE:    return "==";
+            case NE:    return "!=";
+            case GE:    return ">=";
+            case LE:    return "<=";
+            case LT:    return "<";
+            case GT:    return ">";
+            //cast
+            case CAST:  return "cast";
+            case RET:   return "return";
+        }
+    }
+    
+    void print(std::ostream& out, size_t indent=0, std::string pre="") const {
+        out << utils::prefix("\t", indent) << pre << "(";
+        node::print(out);
+        out << " " << expr::string(etype()) << std::endl;
+        if (left()) {
+            left()->print(out, indent+1, pre); 
+            out << std::endl;
+        }
+        if (right()) {
+            right()->print(out, indent+1, pre); 
+            out << std::endl;
+        }
+        out << utils::prefix("\t", indent) << pre << ")";
+    }
+};
+
+class declaration: virtual public node {
+private:
+    std::shared_ptr<meta> t_meta;
+    std::shared_ptr<ID>   id;
+public:
+    declaration(std::shared_ptr<meta> type, std::shared_ptr<ID> name) : node(type) {
+        t_meta = type;
+        id = name;
+    }
+    declaration(std::shared_ptr<meta> type, std::string name) : node(type) {
+        t_meta = type;
+        id = std::make_shared<ID>(name, type);
+    }
+    std::shared_ptr<meta> type() const { return t_meta; }
+    std::shared_ptr<ID> name() const { return id; } 
+
+    void print(std::ostream& out, size_t indent=0, std::string pre="") const {
+        type()->print(out, indent, pre);
+        out << " ";
+        name()->print(out);
+    }
+};
---
+++ src/types.h
@@ -0,0 +1,243 @@
+#include <memory>
+#include <string>
+#include "debug.h"
+#include "utils.h"
+
+typedef enum DataClass {
+    NO_CLASS    = 0,
+    INTEGRAL    = 1,
+    REAL        = 2,
+    COMPLEX     = 3,
+    OBJECT      = 4
+} DataClass;
+
+typedef enum {
+    BOOL, CHAR, U8, I8,
+    U16, I16,
+    U32, I32, R32, X32,
+    U64, I64, R64, X64,
+    VOID,
+    LIST
+} DataType;
+
+class meta {
+private:
+    DataType data_type;
+    DataClass data_class;
+    std::string type_name;
+    std::shared_ptr<meta> c_meta;
+    int t_size;
+    std::shared_ptr<meta> addChild(DataType dt, std::string ts) {
+        if (dt == LIST) {
+            int i = ts.rfind("[");
+            int j = ts.rfind("]");
+            if (j != i+1)
+                t_size = std::stoi(ts.substr(i+1, j-i-1));
+            return std::make_shared<meta>(ts.substr(0, i));
+        }
+        return nullptr;
+    }
+public:
+    meta(DataType dataType, std::string typeName, std::shared_ptr<meta> c_meta = nullptr) {
+        data_type = dataType;
+        data_class = meta::dataClass(dataType);
+        type_name = typeName;
+        //if (!c_meta) debug::log() << "Initialized with nullptr" << std::endl;
+        this->c_meta = c_meta;
+    }
+    meta(std::string typeString) {
+        data_type = typeFromString(typeString);
+        /*if (data_type == STRUCT || data_type == CLASS) 
+            type_name = typeString.substr(0, typeString.length() - 2);
+        else*/ 
+        type_name = typeString;
+        data_class = meta::dataClass(data_type);
+        t_size = 0;
+        c_meta = addChild(data_type, typeString);
+    }
+    meta(std::shared_ptr<meta> c_meta) {
+        data_type = LIST;
+        data_class = meta::dataClass(LIST);
+        type_name = c_meta->name();
+        type_name += "[]";
+        this->c_meta = c_meta;
+    }
+    inline DataType type() const { return data_type; }
+    inline std::string name() const { return type_name; }
+    inline std::shared_ptr<meta> child() const { return c_meta; }
+    inline DataClass dataClass() const { return data_class; }
+    inline int size() const { return t_size; }
+
+    inline void setChild(std::shared_ptr<meta> child) { c_meta = child; }
+    inline void setSize(int sz) { t_size = sz; }
+
+    static std::string string(DataType type) {
+        switch (type) {
+            case BOOL:  return "bool";
+            case CHAR:  return "char";
+            case U8:    return "u8";
+            case I8:    return "i8";
+            case U16:   return "u16";
+            case I16:   return "i16";
+            case U32:   return "u32";
+            case I32:   return "i32";
+            case R32:   return "r32";
+            case X32:   return "x32";
+            case U64:   return "u64";
+            case I64:   return "i64";
+            case R64:   return "r64";
+            case X64:   return "x64";
+            case VOID:  return "void";
+            case LIST:  return "list";
+        }
+    }
+    static DataType typeFromString(std::string s) {
+        if (s.back() == ']')    return LIST;
+        else if (s == "bool")   return BOOL;
+        else if (s == "char")   return CHAR;
+        else if (s == "u8")     return U8;
+        else if (s == "i8")     return I8;
+        else if (s == "u16")    return U16;
+        else if (s == "i16")    return I16;
+        else if (s == "u32")    return U32;
+        else if (s == "i32")    return I32;
+        else if (s == "r32")    return R32;
+        else if (s == "x32")    return X32;
+        else if (s == "u64")    return U64;
+        else if (s == "i64")    return I64;
+        else if (s == "r64")    return R64;
+        else if (s == "x64")    return X64;
+        else if (s == "void")   return VOID;
+        else return VOID;
+    }
+    static DataClass dataClass(DataType type) {
+        switch(type) {
+            case BOOL:
+            case CHAR:
+            case U8:
+            case I8:
+            case U16:
+            case I16:
+            case U32:
+            case I32:
+            case U64:
+            case I64:
+                return INTEGRAL;
+            case R32:
+            case R64:
+                return REAL;
+            case X32:
+            case X64:
+                return COMPLEX;
+            case VOID:
+                return NO_CLASS;
+            default:
+                return OBJECT;
+        }
+    }
+    static int size(DataType type) {
+        switch (type) {
+            case BOOL:
+            case CHAR:
+            case U8:
+            case I8:
+                return 1;
+            case U16:
+            case I16:
+                return 2;
+            case U32:
+            case I32:
+            case R32:
+                return 4;
+            case X32:
+            case U64:
+            case I64:
+            case R64:
+                return 8;
+            case X64:
+                return 16;
+            default:
+                return 0;
+        }
+    }
+
+    std::string short_string() {
+        switch (data_type) {
+            case BOOL:  return "b";
+            case CHAR:  return "c";
+            case U8:    return "u8";
+            case I8:    return "i8";
+            case U16:   return "u16";
+            case I16:   return "i16";
+            case U32:   return "u32";
+            case I32:   return "i32";
+            case R32:   return "r32";
+            case X32:   return "x32";
+            case U64:   return "u64";
+            case I64:   return "i64";
+            case R64:   return "r64";
+            case X64:   return "x64";
+            case VOID:  return "v";
+            case LIST:  return child()->short_string() + "l";
+        }
+    }
+    std::string to_string() {
+        if (type() == LIST) {
+            return child()->to_string() + "[" + std::to_string(size()) + "]";
+        }
+        /*
+        else if (type() == POINTER){
+            return child()->to_string() + "*";
+        }
+        else if (type() == CLASS)
+            return name() + "()";
+        else if (type() == STRUCT)
+            return name() + "{}";*/
+        else return string(type());
+    }
+    void print(std::ostream& out, size_t indent = 0, std::string pre = "") const {
+        out << utils::prefix("\t", indent) << pre;
+        //debug::log() << "meta print!" << std::endl;
+        //debug::log() << "asdlfjk222" << std::endl;
+        //debug::log() << meta::string(type()) << std::endl;
+        //debug::log() << "asdlfjk" << std::endl;
+        if (type() == LIST) {
+            //debug::log() << "meta array!" << std::endl;
+            if (child())
+                child()->print(out, indent, pre);
+            else out << "list";
+            out << "[" << size() << "]";
+        }/*
+        else if (type() == POINTER){
+            child()->print(out, indent, pre);
+            out << "*";
+        }
+        else if (type() == CLASS)
+            out << name() + "()";
+        else if (type() == STRUCT)
+            out << name() + "{}";*/
+        else out << meta::string(type());
+    }
+
+    friend bool operator==(const meta& l, const meta& r) {
+        bool t = l.type() == r.type();
+        if (t && (l.type() == LIST /*|| l.type() == POINTER*/))
+            return *l.child() == *r.child();
+        /*
+        if (t && (l.type() == STRUCT || l.type() == CLASS))
+            return l.name() == r.name();*/
+        return t;
+    }
+    friend bool operator!=(const meta& l, const meta& r) { return !(l == r); }
+    friend std::ostream& operator<<(std::ostream& o, const meta& m) {
+        m.print(o);
+        return o;
+    }
+    static bool canCast(std::shared_ptr<meta> to, std::shared_ptr<meta> from) {
+        return 
+            from->dataClass() != OBJECT
+            && to->dataClass() != OBJECT 
+            && from->dataClass() <= to->dataClass() 
+            && meta::size(from->type()) <= meta::size(to->type());
+    }
+};

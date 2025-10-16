57c7988b2a2e43d760e97a50a3eab2f5d2c4de87
Aidan Jones <aejones4gm@gmail.com>
2025-10-16 17:22:36 -0500

    chore: move gen scripts to subfolder

--- parser/scripts/generate-types.ts
+++ parser/scripts/generate-types.ts
@@ -1,10 +1,10 @@
 import { generateCstDts } from 'chevrotain';
 import { writeFileSync } from 'fs';
 import { join } from 'path';
-import { parser } from './parser.js';
+import { parser } from '../parser.js';
 
 const types = generateCstDts(parser.getGAstProductions());
-const path = join('./cst-types.ts');
+const path = join('../cst-types.ts');
 writeFileSync(path, types);
 console.log(
   `Generated ${types.match(/export (type|interface)/gi)?.length ?? 0} types and wrote them to ${path}`

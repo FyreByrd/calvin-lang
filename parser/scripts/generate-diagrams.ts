e5663435871244a70c94ff51b67396136e78ee98
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2025-10-18 22:10:37 -0500

    chore: remove old parser project
    Remove the original parser workspace, completing the move to the monorepo

--- parser/scripts/generate-diagrams.ts
+++
@@ -1,9 +0,0 @@
-import { createSyntaxDiagramsCode } from 'chevrotain';
-import { writeFileSync } from 'fs';
-import { join } from 'path';
-import { parser } from '../parser.js';
-
-const diagrams = createSyntaxDiagramsCode(parser.getSerializedGastProductions());
-const path = join(import.meta.dirname, '../../docs/syntax-diagrams.html');
-writeFileSync(path, diagrams);
-console.log(`Wrote output to ${path}`);

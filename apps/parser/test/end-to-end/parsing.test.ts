af6b63ac3821a32c0dfb9326d7245a996071c48e
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2025-10-18 21:25:39 -0500

    test: add end-to-end tests

---
+++ apps/parser/test/end-to-end/parsing.test.ts
@@ -0,0 +1,43 @@
+import * as TestSubject from '@calvin-lang/parser/lib';
+import { bold, dim, yellow } from '@std/fmt/colors';
+import { walk } from '@std/fs';
+import { toFileUrl } from '@std/path';
+import { performParsingTestCase, useGlobalSettings } from '@/test/utils/mod.ts';
+
+Deno.test('Loading & parsing', async (t) => {
+  const snippetUrl = new URL(import.meta.resolve('./fixtures'));
+
+  for await (const file of walk(snippetUrl)) {
+    if (!(file.isFile && file.name.endsWith('.txt'))) {
+      const warningMsg = [
+        `${bold(yellow('Warning:'))} ${bold(file.name)} is not a .txt file.`,
+        `${bold(dim('Skipping'))} ${dim(`${file.path}...`)}`,
+      ].join('\n');
+      console.warn(warningMsg);
+
+      continue;
+    }
+
+    await t.step(file.name, async () => {
+      using _globalSettings = useGlobalSettings({ debugTrees: false });
+
+      const parser = new TestSubject.CalvinParser();
+
+      const precedenceHandler = new TestSubject.PrecedenceHandler();
+
+      const printer = new TestSubject.CalvinPrinter();
+
+      const typeAnalyzer = new TestSubject.CalvinTypeAnalyzer();
+
+      const _testCaseOutputs = performParsingTestCase({
+        code: await Deno.readTextFile(toFileUrl(file.path)),
+        parser,
+        precedenceHandler,
+        printer,
+        typeAnalyzer,
+      });
+
+      // TODO - Add file-specific assertion logic
+    });
+  }
+});

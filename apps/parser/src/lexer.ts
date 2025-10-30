304b07494ba77aee009b276038f5006b0bc932e1
Aidan Jones <aejones4gm@gmail.com>
2025-10-30 10:00:36 -0500

    feat(lexer): add brackets

--- apps/parser/src/lexer.ts
+++ apps/parser/src/lexer.ts
@@ -210,6 +210,8 @@ export const LPAREN: TokenType = createToken({ name: 'LPAREN', pattern: '(' });
 export const RPAREN: TokenType = createToken({ name: 'RPAREN', pattern: ')' });
 export const LCURLY: TokenType = createToken({ name: 'LCURLY', pattern: '{' });
 export const RCURLY: TokenType = createToken({ name: 'RCURLY', pattern: '}' });
+export const LBRACK: TokenType = createToken({ name: 'LBRACK', pattern: '[' });
+export const RBRACK: TokenType = createToken({ name: 'RBRACK', pattern: ']' });
 export const SEMI: TokenType = createToken({ name: 'SEMI', pattern: ';' });
 export const COLON: TokenType = createToken({ name: 'COLON', pattern: ':' });
 /* Keywords */
@@ -269,6 +271,8 @@ export const allTokens: TokenType[] = [
   RPAREN,
   LCURLY,
   RCURLY,
+  LBRACK,
+  RBRACK,
   SEMI,
   COLON,
   ERROR,

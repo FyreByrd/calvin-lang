2176a01a3c1c19aca683a5f22379f6f3361c2ad8
Aidan Jones <aejones4gm@gmail.com>
2025-10-30 18:30:31 -0500

    chore(parser): update cst-types

--- apps/parser/generated/cst-types.ts
+++ apps/parser/generated/cst-types.ts
@@ -134,7 +134,7 @@ export type ConstantCstChildren = {
   INT?: IToken[];
   LBRACK?: IToken[];
   expression?: ExpressionCstNode[];
-  SEP?: IToken[];
+  COMMA?: IToken[];
   RBRACK?: IToken[];
 };
 

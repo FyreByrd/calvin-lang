9ca1c60ecb420c81869b87fbaf9420200f4f14c4
Aidan Jones <aejones4gm@gmail.com>
2025-11-06 22:09:15 -0600

    feat(printers): bring JSON printer to parity with JSON.stringify

--- apps/parser/src/visitors/printers/json.ts
+++ apps/parser/src/visitors/printers/json.ts
@@ -19,34 +19,40 @@ const start = ANSIColor.BrightRed;
 const range = ANSIColor.BrightWhite - start;
 
 export class JSONPrinter extends BasePrinter implements ICstNodeVisitor<number, void> {
-  constructor(colors: boolean = true, output: Logger | null = console.log) {
+  private readonly pretty: string;
+  constructor(
+    colors: boolean = true,
+    output: Logger | null = console.log,
+    private readonly indentLevel: number = 2,
+  ) {
     super(colors, output);
+    this.pretty = this.indentLevel ? ' ' : '';
   }
 
   private tree(msg: string, indent: number, trail: boolean = false) {
     if (this.colors) {
       this.output?.(
-        color(start + ((indent / 2) % range), prefix(`${msg}${trail ? ',' : ''}`, indent)),
+        color(start + ((indent) % range), prefix(`${msg}${trail ? ',' : ''}`, indent * this.indentLevel)),
       );
     } else {
-      this.output?.(prefix(`${msg}${trail ? ',' : ''}`, indent));
+      this.output?.(prefix(`${msg}${trail ? ',' : ''}`, indent * this.indentLevel));
     }
   }
 
   file(node: FileCstChildren, indent: number, trail: boolean = false) {
     this.tree('{', indent);
-    this.tree('"file": {', indent + 2);
+    this.tree(`"file":${this.pretty}{`, indent + 1);
     if (node.statement) {
-      this.statement_list(node.statement, indent + 4);
+      this.statement_list(node.statement, indent + 2);
     }
-    this.tree('}', indent + 2);
+    this.tree('}', indent + 1);
     this.tree('}', indent, trail);
   }
 
   statement_list(statements: StatementCstNode[], indent: number, trail: boolean = false) {
-    this.tree('"statements": [', indent);
+    this.tree(`"statements":${this.pretty}[`, indent);
     for (let i = 0; i < statements.length; i++) {
-      this.statement(statements[i].children, indent + 2, i + 1 !== statements.length);
+      this.statement(statements[i].children, indent + 1, i + 1 !== statements.length);
     }
     this.tree(']', indent, trail);
   }
@@ -54,63 +60,63 @@ export class JSONPrinter extends BasePrinter implements ICstNodeVisitor<number, 
   statement(stmt: StatementCstChildren, indent: number, trail: boolean = false) {
     this.tree('{', indent);
     if (stmt.declaration) {
-      this.tree('"type": "declaration"', indent + 2, true);
-      this.declaration(stmt.declaration[0].children, indent + 2);
+      this.tree(`"type":${this.pretty}"declaration"`, indent + 1, true);
+      this.declaration(stmt.declaration[0].children, indent + 1);
     } else if (stmt.BREAK) {
-      this.tree('"type": "break"', indent + 2);
+      this.tree(`"type":${this.pretty}"break"`, indent + 1);
     } else if (stmt.CONTINUE) {
-      this.tree('"type": "continue"', indent + 2);
+      this.tree(`"type":${this.pretty}"continue"`, indent + 1);
     } else if (stmt.RETURN) {
-      this.tree('"type": "return"', indent + 2, !!stmt.expression);
+      this.tree(`"type":${this.pretty}"return"`, indent + 1, !!stmt.expression);
       if (stmt.expression) {
-        this.expression(stmt.expression[0].children, indent + 2);
+        this.expression(stmt.expression[0].children, indent + 1);
       }
     } else if (stmt.IF && stmt.ifPredBody) {
-      this.tree('"type": "if"', indent + 2, true);
+      this.tree(`"type":${this.pretty}"if"`, indent + 1, true);
       let bodyCount = 0;
       const ifPredBody = stmt.ifPredBody;
-      this.ifPredBody(ifPredBody[bodyCount++].children, indent + 2, !!(stmt.ELIF || stmt.ELSE));
+      this.ifPredBody(ifPredBody[bodyCount++].children, indent + 1, !!(stmt.ELIF || stmt.ELSE));
       if (stmt.ELIF) {
-        this.tree('"elif": [', indent + 2);
+        this.tree(`"elif":${this.pretty}[`, indent + 1);
         for (let i = 0; i < stmt.ELIF.length; i++) {
-          this.tree('{', indent + 4);
-          this.ifPredBody(ifPredBody[bodyCount++].children, indent + 6);
-          this.tree('}', indent + 4, i + 1 !== stmt.ELIF.length);
+          this.tree('{', indent + 2);
+          this.ifPredBody(ifPredBody[bodyCount++].children, indent + 3);
+          this.tree('}', indent + 2, i + 1 !== stmt.ELIF.length);
         }
-        this.tree(']', indent + 2, !!stmt.ELSE);
+        this.tree(']', indent + 1, !!stmt.ELSE);
       }
       if (stmt.ELSE && stmt.body) {
-        this.tree('"else": {', indent + 2);
-        this.body(stmt.body[0].children, indent + 4);
-        this.tree('}', indent + 2);
+        this.tree(`"else":${this.pretty}{`, indent + 1);
+        this.body(stmt.body[0].children, indent + 2);
+        this.tree('}', indent + 1);
       }
     } else if (stmt.WHILE && stmt.expression) {
       let bodyCount = 0;
-      this.tree('"type": "while"', indent + 2, true);
+      this.tree(`"type":${this.pretty}"while"`, indent + 1, true);
       if (stmt.DO && stmt.body) {
-        this.tree('"do": {', indent + 2);
-        this.body(stmt.body[bodyCount++].children, indent + 4);
-        this.tree('}', indent + 2, true);
+        this.tree(`"do":${this.pretty}{`, indent + 1);
+        this.body(stmt.body[bodyCount++].children, indent + 2);
+        this.tree('}', indent + 1, true);
       }
-      this.expression(stmt.expression[0].children, indent + 2, true);
+      this.expression(stmt.expression[0].children, indent + 1, true);
       if (!stmt.SEMI && stmt.body) {
-        this.body(stmt.body[bodyCount++].children, indent + 2, !!stmt.FINALLY);
+        this.body(stmt.body[bodyCount++].children, indent + 1, !!stmt.FINALLY);
       } else {
-        this.tree('"body": {}', indent + 2, !!stmt.FINALLY);
+        this.tree(`"body":${this.pretty}{}`, indent + 1, !!stmt.FINALLY);
       }
       if (stmt.FINALLY && stmt.body) {
-        this.tree('"finally": {', indent + 2);
-        this.body(stmt.body[bodyCount++].children, indent + 4);
-        this.tree('}', indent + 2);
+        this.tree(`"finally":${this.pretty}{`, indent + 1);
+        this.body(stmt.body[bodyCount++].children, indent + 2);
+        this.tree('}', indent + 1);
       }
     } else if (stmt.body) {
-      this.tree('"type": "body",', indent + 2);
-      this.body(stmt.body[0].children, indent + 2);
+      this.tree(`"type":${this.pretty}"body",`, indent + 1);
+      this.body(stmt.body[0].children, indent + 1);
     } else if (stmt.expression) {
-      this.tree('"type": "expression",', indent + 2);
-      this.expression(stmt.expression[0].children, indent + 2);
+      this.tree(`"type":${this.pretty}"expression",`, indent + 1);
+      this.expression(stmt.expression[0].children, indent + 1);
     } else {
-      this.tree('"type": "empty"', indent + 2);
+      this.tree(`"type":${this.pretty}"empty"`, indent + 1);
     }
     this.tree('}', indent, trail);
   }
@@ -125,32 +131,32 @@ export class JSONPrinter extends BasePrinter implements ICstNodeVisitor<number, 
   }
 
   declaration(decl: DeclarationCstChildren, indent: number, trail: boolean = false) {
-    this.tree(`"declaration": {`, indent);
-    this.tree(`"image": "${decl.ID[0].image}"`, indent + 2, !!(decl.type || decl.expression));
+    this.tree(`"declaration":${this.pretty}{`, indent);
+    this.tree(`"image":${this.pretty}"${decl.ID[0].image}"`, indent + 1, !!(decl.type || decl.expression));
     if (decl.type) {
-      this.type(decl.type[0].children, indent + 2, !!decl.expression);
+      this.type(decl.type[0].children, indent + 1, !!decl.expression);
     }
     if (decl.expression) {
-      this.expression(decl.expression[0].children, indent + 2, false);
+      this.expression(decl.expression[0].children, indent + 1, false);
     }
     this.tree('}', indent, trail);
   }
 
   body(body: BodyCstChildren, indent: number, trail: boolean = false) {
-    this.tree('"body": {', indent);
+    this.tree(`"body":${this.pretty}{`, indent);
     if (body.statement) {
-      this.statement_list(body.statement, indent + 2);
+      this.statement_list(body.statement, indent + 1);
     }
     this.tree('}', indent, trail);
   }
 
   expression(expr: ExpressionCstChildren, indent: number, trail: boolean = false) {
     const op = Object.values(expr).find((e) => 'tokenType' in e[0]) as IToken[] | undefined;
-    this.tree(`"expression": {`, indent);
-    this.tree(`"op": "${op?.[0].image ?? ''}"`, indent + 2, true);
-    this.value(expr.value[0].children, indent + 2, !!expr.expression);
+    this.tree(`"expression":${this.pretty}{`, indent);
+    this.tree(`"op":${this.pretty}"${op?.[0].image ?? ''}"`, indent + 1, true);
+    this.value(expr.value[0].children, indent + 1, !!expr.expression);
     if (expr.expression) {
-      this.expression(expr.expression[0].children, indent + 2);
+      this.expression(expr.expression[0].children, indent + 1);
     }
     this.tree('}', indent, trail);
   }
@@ -158,17 +164,17 @@ export class JSONPrinter extends BasePrinter implements ICstNodeVisitor<number, 
   value(val: ValueCstChildren, indent: number, trail: boolean = false) {
     this.tree('"value": {', indent);
     if (val.expression) {
-      this.tree('"nested": {', indent + 2);
-      this.expression(val.expression[0].children, indent + 4);
-      this.tree('}', indent + 2);
+      this.tree(`"nested":${this.pretty}{`, indent + 1);
+      this.expression(val.expression[0].children, indent + 2);
+      this.tree('}', indent + 1);
     } else if (val.constant) {
-      this.constant(val.constant[0].children, indent + 2);
+      this.constant(val.constant[0].children, indent + 1);
     } else if (val.ID) {
-      this.tree(`"id": "${val.ID[0].image}"`, indent);
+      this.tree(`"id":${this.pretty}"${val.ID[0].image}"`, indent);
     } else if (val.value) {
       const op = Object.values(val).find((v) => 'tokenType' in v[0]) as IToken[];
-      this.tree(`"prefix": "${op[0].image}"`, indent + 2, true);
-      this.value(val.value[0].children, indent + 2);
+      this.tree(`"prefix":${this.pretty}"${op[0].image}"`, indent + 1, true);
+      this.value(val.value[0].children, indent + 1);
     } else {
       throw new Error(`TypeInference: unhandled value type ${JSON.stringify(val)}`);
     }
@@ -176,11 +182,11 @@ export class JSONPrinter extends BasePrinter implements ICstNodeVisitor<number, 
   }
 
   constant(c: ConstantCstChildren, indent: number, trail: boolean = false) {
-    this.tree(`"constant": "${Object.values(c)[0][0].image}"`, indent, trail);
+    this.tree(`"constant":${this.pretty}"${Object.values(c)[0][0].image}"`, indent, trail);
   }
 
   type(t: TypeCstChildren, indent: number, trail: boolean = false) {
-    this.tree(`"type": "${t.BASIC_TYPE[0].image}"`, indent, trail);
+    this.tree(`"type":${this.pretty}"${t.BASIC_TYPE[0].image}"`, indent, trail);
   }
 
   override visit(node: CstNode, indent: number = 0) {

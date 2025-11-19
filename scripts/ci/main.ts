2464c9fd19b94f3377e1cc37d67e4a2afb180ab7
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2025-11-19 16:09:41 -0600

    refactor(infra/actions): fix CI detection to actually choose a branch, and also inspect the CI variable

--- scripts/ci/main.ts
+++ scripts/ci/main.ts
@@ -2,11 +2,13 @@ import { notice } from '@actions/core';
 import { bold } from '@std/fmt/colors';
 
 export async function main(): Promise<void> {
-  if (Deno.env.get('CI')) {
-    console.log("We're in a CI environment!");
-  }
+  const ci = Deno.env.get('CI');
 
-  console.log(`We are ${bold('not')} in a CI environment!`);
+  if (ci) {
+    console.log("We're in a CI environment!", { ci });
+  } else {
+    console.log(`We are ${bold('not')} in a CI environment!`);
+  }
 }
 
 if (import.meta.main) {
